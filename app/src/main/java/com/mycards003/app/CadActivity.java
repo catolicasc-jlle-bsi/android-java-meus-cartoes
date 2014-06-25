a214e39516455c1abe5378dd5c7c7bfd8a2fe871
Renan Yuhara <renan.takeshi@gmail.com>
2014-06-25 09:49:43 -0300

    Primeiro commit

---
+++ app/src/main/java/com/mycards003/app/CadActivity.java
@@ -0,0 +1,52 @@
+package com.mycards003.app;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Takeshi on 24/06/2014.
+ */
+public class CadActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Intent intent = getIntent();
+        //String message = intent.getStringExtra(MainActivity.)
+
+        setContentView(R.layout.activity_cad);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new MainActivity.PlaceholderFragment()).commit();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static class PlaceHolderFragment extends Fragment {
+        public PlaceHolderFragment() {
+
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.activity_cad,container, false);
+            return rootView;
+        }
+    }
+
+}
